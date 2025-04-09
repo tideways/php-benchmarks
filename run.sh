031f1f2a528ae5aa95e10bd2731ec8a63cd1fb71
Tim Düsterhus <tim@tideways-gmbh.com>
2025-04-09 13:56:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-for v in 82 83 84; do
-	echo "GET http://localhost:81$v/index.php" |vegeta attack -name=$v -duration=60s -rate=250/1s |tee results.$v.bin |vegeta report
+
+set -ex
+
+for project in symfony7demo laravel12 wordpress67; do
+	for v in 82 83 84; do
+		hostname="$project-$v"
+
+		case $project in
+
+		symfony7demo)
+			path="http://${hostname}.localhost/index.php/en/blog/"
+			;;
+
+		laravel12 | wordpress67)
+			path="http://${hostname}.localhost/"
+			;;
+
+		*)
+			echo "Bug"
+			exit 1
+			;;
+		esac
+
+		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=5s -rate=100/1s > /dev/null
+		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=60s -rate=250/1s |tee "results.${hostname}.bin" |vegeta report
+
+	done
 done
-
-vegeta plot *.bin > plot.html
-
