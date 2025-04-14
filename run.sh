032e1bb2320c7d3b372888a25e030067823becea
Tim Düsterhus <tim@tideways-gmbh.com>
2025-04-14 10:41:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,28 @@
 set -ex
 
 for project in symfony7demo laravel12 wordpress67; do
-	for v in 82 83 84; do
+	systemctl restart php8.4-fpm
+	systemctl restart php8.3-fpm
+	systemctl restart php8.2-fpm
+	systemctl restart php7.4-fpm
+
+	case $project in
+
+	symfony7demo | laravel12)
+		versions="82 83 84"
+		;;
+
+	wordpress67)
+		versions="74 82 83 84"
+		;;
+
+	*)
+		echo "Bug"
+		exit 1
+		;;
+	esac
+
+	for v in $versions; do
 		hostname="$project-$v"
 
 		case $project in
@@ -22,8 +43,10 @@ for project in symfony7demo laravel12 wordpress67; do
 			;;
 		esac
 
-		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=5s -rate=100/1s > /dev/null
-		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=60s -rate=250/1s |tee "results.${hostname}.bin" |vegeta report
+		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=5s -rate=30/1s > /dev/null
+		echo "GET $path" |vegeta attack "-name=${hostname}" -duration=60s -rate=50/1s |tee "results.${hostname}.bin" |vegeta report
 
 	done
+
+	sleep 3
 done
