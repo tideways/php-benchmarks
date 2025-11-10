f304af424bf0f4edb5cdf6180859dd72b2bbab25
Tim Düsterhus <tim@tideways-gmbh.com>
2025-11-10 09:32:36 +0100

    Generate symfony7demo `.env.local` in prepare.sh

--- symfony7demo/prepare.sh
+++ symfony7demo/prepare.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+cat << "EOT" > .env.local
+# This variable is for the demo app only!
+# See https://symfony.com/doc/current/reference/configuration/framework.html#configuration-framework-secret
+APP_SECRET=not-a-secret--read-the-doc!
+EOT
 composer install
 composer dump-env prod
 composer dump-autoload --optimize
